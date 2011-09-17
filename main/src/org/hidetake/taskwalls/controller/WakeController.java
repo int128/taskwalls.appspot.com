b4fb9845bde01a298e773fb298124c1565cb27b5
int128 <int128@gmail.com>
2011-09-17 16:48:14 +0900

    new

---
+++ main/src/org/hidetake/taskwalls/controller/WakeController.java
@@ -0,0 +1,15 @@
+package org.hidetake.taskwalls.controller;
+
+import org.slim3.controller.Controller;
+import org.slim3.controller.Navigation;
+
+public class WakeController extends Controller
+{
+
+	@Override
+	public Navigation run() throws Exception
+	{
+		return null;
+	}
+
+}
