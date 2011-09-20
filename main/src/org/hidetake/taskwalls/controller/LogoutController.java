4a93da1adce3bb71acdcd3db52ed3926d9b07015
int128 <int128@gmail.com>
2011-09-21 01:06:56 +0900

    fix bug for unauthorized state

--- main/src/org/hidetake/taskwalls/controller/LogoutController.java
+++ main/src/org/hidetake/taskwalls/controller/LogoutController.java
@@ -1,7 +1,11 @@
 package org.hidetake.taskwalls.controller;
 
+import java.util.logging.Logger;
+
 import javax.servlet.http.Cookie;
 
+import org.hidetake.taskwalls.util.StackTraceUtil;
+import org.slim3.controller.Controller;
 import org.slim3.controller.Navigation;
 import org.slim3.memcache.Memcache;
 
@@ -9,16 +13,39 @@ import org.slim3.memcache.Memcache;
  * Logout current session.
  * @author hidetake.org
  */
-public class LogoutController extends ControllerBase
+public class LogoutController extends Controller
 {
+
+	private static final Logger logger = Logger.getLogger(LogoutController.class.getName());
 
 	@Override
 	public Navigation run() throws Exception
 	{
-		Memcache.delete(sessionKey);
-		Cookie sessionCookie = new Cookie(COOKIE_KEY_SESSIONID, "");
-		sessionCookie.setMaxAge(0);
-		response.addCookie(sessionCookie);
+		if (request.getCookies() != null) {
+			String sessionKey = null;
+			for (Cookie cookie : request.getCookies()) {
+				if (ControllerBase.COOKIE_KEY_SESSIONID.equals(cookie.getName())) {
+					sessionKey = cookie.getValue();
+					break;
+				}
+			}
+			if (sessionKey != null) {
+				// delete the session
+				Memcache.delete(sessionKey);
+			}
+			// delete the cookie
+			Cookie sessionCookie = new Cookie(ControllerBase.COOKIE_KEY_SESSIONID, "");
+			sessionCookie.setMaxAge(0);
+			response.addCookie(sessionCookie);
+		}
+		return redirect("./");
+	}
+
+	@Override
+	protected Navigation handleError(Throwable error) throws Throwable
+	{
+		// redirect anyway
+		logger.severe(StackTraceUtil.format(error));
 		return redirect("./");
 	}
 
