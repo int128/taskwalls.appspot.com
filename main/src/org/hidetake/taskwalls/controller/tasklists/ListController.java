38243f085083d0c9ef65745722bd00be7fd2b631
int128 <int128@gmail.com>
2011-09-23 00:59:07 +0900

    implemented loading tasklist options

--- main/src/org/hidetake/taskwalls/controller/tasklists/ListController.java
+++ main/src/org/hidetake/taskwalls/controller/tasklists/ListController.java
@@ -1,10 +1,14 @@
 package org.hidetake.taskwalls.controller.tasklists;
 
 import org.hidetake.taskwalls.controller.ControllerBase;
+import org.hidetake.taskwalls.meta.TasklistOptionsMeta;
+import org.hidetake.taskwalls.model.TasklistOptions;
+import org.hidetake.taskwalls.service.TasklistOptionsService;
 import org.hidetake.taskwalls.util.AjaxPreconditions;
 import org.hidetake.taskwalls.util.JsonCache;
 import org.slim3.controller.Navigation;
 
+import com.google.api.services.tasks.model.TaskList;
 import com.google.api.services.tasks.model.TaskLists;
 import com.google.appengine.api.memcache.Expiration;
 
@@ -36,6 +40,13 @@ public class ListController extends ControllerBase
 			entry.put(taskLists);
 		}
 
+		TasklistOptionsMeta m = TasklistOptionsMeta.get();
+		for (TaskList taskList : taskLists.getItems()) {
+			TasklistOptions tasklistOptions = TasklistOptionsService.get(taskList.getId());
+			if (tasklistOptions != null) {
+				taskList.put(m.colorID.getName(), tasklistOptions.getColorID());
+			}
+		}
 		return jsonResponse(taskLists);
 	}
 
--- main/src/org/hidetake/taskwalls/service/TasklistOptionsService.java
+++ main/src/org/hidetake/taskwalls/service/TasklistOptionsService.java
@@ -1,5 +1,6 @@
 package org.hidetake.taskwalls.service;
 
+import org.hidetake.taskwalls.meta.TasklistOptionsMeta;
 import org.hidetake.taskwalls.model.TasklistOptions;
 import org.slim3.datastore.Datastore;
 
@@ -17,6 +18,20 @@ public class TasklistOptionsService
 	public static void put(TasklistOptions tasklistOptions)
 	{
 		Datastore.put(tasklistOptions);
+	}
+
+	/**
+	 * Gets the {@link TasklistOptions}.
+	 * @param id tasklist ID
+	 * @return the model or null if not exists
+	 */
+	public static TasklistOptions get(String id)
+	{
+		if (id == null) {
+			throw new NullPointerException("id is null");
+		}
+		TasklistOptionsMeta m = TasklistOptionsMeta.get();
+		return Datastore.getOrNull(m, TasklistOptions.createKey(id));
 	}
 
 }
