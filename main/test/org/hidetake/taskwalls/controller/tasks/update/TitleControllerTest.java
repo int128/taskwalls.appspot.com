6cebfc3d384fa8f3362933d13b6710a6fb40c9db
int128 <int128@gmail.com>
2011-09-26 09:08:29 +0900

    fixed test failure

--- main/test/org/hidetake/taskwalls/controller/tasks/update/TitleControllerTest.java
+++ main/test/org/hidetake/taskwalls/controller/tasks/update/TitleControllerTest.java
@@ -1,10 +1,11 @@
 package org.hidetake.taskwalls.controller.tasks.update;
 
-import org.slim3.tester.ControllerTestCase;
 import org.junit.Test;
-import static org.junit.Assert.*;
+import org.slim3.tester.ControllerTestCase;
 
 import static org.hamcrest.CoreMatchers.*;
+
+import static org.junit.Assert.*;
 
 public class TitleControllerTest extends ControllerTestCase
 {
@@ -13,7 +14,7 @@ public class TitleControllerTest extends ControllerTestCase
 	public void noSession() throws Exception
 	{
 		tester.start("/tasks/update/title");
-		StatusController controller = tester.getController();
+		TitleController controller = tester.getController();
 		assertThat(controller, is(notNullValue()));
 		assertThat(tester.isRedirect(), is(false));
 		assertThat(tester.getDestinationPath(), is("/errors/noSession"));
