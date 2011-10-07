c364b2d2ee943df0d43a71149d05e4b0f1b94052
int128 <int128@gmail.com>
2011-10-07 22:15:30 +0900

    implements creating task API

---
+++ main/src/org/hidetake/taskwalls/controller/tasks/CreateController.java
@@ -0,0 +1,47 @@
+package org.hidetake.taskwalls.controller.tasks;
+
+import org.hidetake.taskwalls.controller.ControllerBase;
+import org.hidetake.taskwalls.model.TaskExtension;
+import org.hidetake.taskwalls.util.AjaxPreconditions;
+import org.slim3.controller.Navigation;
+import org.slim3.controller.validator.Validators;
+
+import com.google.api.client.util.DateTime;
+import com.google.api.services.tasks.model.Task;
+
+public class CreateController extends ControllerBase
+{
+
+	@Override
+	public Navigation run() throws Exception
+	{
+		if (!isPost()) {
+			return forward("/errors/preconditionFailed");
+		}
+		if (!AjaxPreconditions.isXHR(request)) {
+			return forward("/errors/preconditionFailed");
+		}
+		if (!validate()) {
+			return forward("/errors/preconditionFailed");
+		}
+
+		Task task = new Task();
+		task.setTitle(asString("title"));
+		task.setNotes(asString("notes"));
+		task.setDue(new DateTime(asLong("dueTime"), 0));
+
+		Task created = tasksService.tasks.insert(asString("tasklistID"), task).execute();
+		TaskExtension.extend(created);
+		return jsonResponse(created);
+	}
+
+	private boolean validate()
+	{
+		Validators v = new Validators(request);
+		v.add("tasklistID", v.required());
+		v.add("title", v.required());
+		v.add("dueTime", v.required(), v.longType());
+		return v.validate();
+	}
+
+}
