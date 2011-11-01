d5f9a971d185a016ffb92209d22c7a975933f003
int128 <int128@gmail.com>
2011-11-02 01:33:38 +0900

    implements preload of the default tasklist.

--- webapp/js/view.js
+++ webapp/js/view.js
@@ -12,17 +12,55 @@ function UIPage () {
  */
 UIPage.prototype.refresh = function () {
 	var context = this;
+	var defaultTasklistID = undefined;
+	var loadedTasklists = undefined;
+	this.calendar.clear();
+	/**
+	 * Load tasks of the default tasklist.
+	 * @param {Tasks} tasks
+	 */
+	Tasks.get('@default', function (tasks) {
+		if (tasks.items.length > 0) {
+			defaultTasklistID = tasks.items[0].tasklistID;
+		}
+		else {
+			defaultTasklistID = '@default';
+		}
+		context.calendar.add(tasks);
+		context.loadOtherTasks(loadedTasklists, defaultTasklistID);
+	});
+	/**
+	 * Load tasklists.
+	 * @param {Tasklists} tasklists
+	 */
 	Tasklists.get(function (tasklists) {
+		loadedTasklists = tasklists;
+		context.calendar.setTasklists(tasklists);
 		context.tasklists.clear();
 		context.tasklists.add(tasklists);
-		context.calendar.clear();
-		context.calendar.setTasklists(tasklists);
-		$.each(tasklists.items, function (i, tasklist) {
-			Tasks.get(tasklist.id, function (tasks) {
-				context.calendar.add(tasks);
-			});
-		});
+		context.loadOtherTasks(loadedTasklists, defaultTasklistID);
 	});
+};
+/**
+ * Load tasks except tasks of the default tasklist.
+ * @param {Tasklists} tasklists
+ * @param {String} defaultTasklistID
+ */
+UIPage.prototype.loadOtherTasks = function (tasklists, defaultTasklistID) {
+	var context = this;
+	if (defaultTasklistID && tasklists) {
+		$.each(tasklists.items, function (i, tasklist) {
+			if (tasklist.id == defaultTasklistID) {
+				$('.tasklist-' + tasklist.id)
+					.addClass('tasklistcolor-' + tasklists.getByID(tasklist.id).colorID);
+			}
+			else {
+				Tasks.get(tasklist.id, function (tasks) {
+					context.calendar.add(tasks);
+				});
+			}
+		});
+	}
 };
 /**
  * @class UI element of the header bar.
