3acf2dbe4cf8468d0fed8bce1584df3b22578b59
int128 <int128@gmail.com>
2011-12-14 02:14:38 +0900

    use jQuery CDN instead

--- webapp/js/app.localhost.js
+++ webapp/js/app.localhost.js
@@ -3,13 +3,11 @@
 		document.write('<'+'script type="text/javascript" src="'+s[i]+'"></'+'script'+'>');
 	} 
 })([
-'/js/lib/jquery-1.7.1.js',
-'/js/lib/jquery-ui-1.8.16.js',
+'/js/src/app.js',
 '/js/src/jquery.cookie.js',
 '/js/src/util.js',
 '/js/src/model.js',
 '/js/src/view.page.js',
 '/js/src/view.calendar.js',
-'/js/src/view.header.js',
-'/js/src/app.js'
+'/js/src/view.header.js'
 ]);
