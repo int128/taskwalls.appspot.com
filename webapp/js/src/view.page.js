df9dfde9e43acb077cd173e3fb9e6f80e16c6842
int128 <int128@gmail.com>
2012-05-28 15:17:16 +0900

    format source

--- webapp/js/src/view.page.js
+++ webapp/js/src/view.page.js
@@ -1,4 +1,7 @@
-var PageViewModel = function () {
+/**
+ * @class PageViewModel
+ */
+var PageViewModel = function() {
 	var self = this;
 
 	// tasks
@@ -6,13 +9,13 @@ var PageViewModel = function () {
 
 	// calendar
 	this.calendar = new CalendarViewModel(this.taskdata);
-	
+
 	// offline
 	this.offline = ko.computed({
-		read: function () {
+		read : function() {
 			return AppSettings.isOffline();
 		},
-		write: function (value) {
+		write : function(value) {
 			AppSettings.setOffline(value);
 		}
 	});
@@ -23,106 +26,19 @@ var PageViewModel = function () {
 	this.oauth2authorizing = ko.observable(false);
 	this.oauth2unauthorized = ko.observable(false);
 	this.oauth2authorizationURL = ko.observable();
-	new OAuth2Session(function () {
-		this.onAuthorized = function () {
+	new OAuth2Session(function() {
+		this.onAuthorized = function() {
 			self.oauth2authorized(true);
 			self.taskdata.load();
 		};
-		this.onAuthorizing = function () {
+		this.onAuthorizing = function() {
 			self.oauth2authorizing(true);
 			// clean up cache
 			localStorage.clear();
 		};
-		this.onUnauthorized = function () {
+		this.onUnauthorized = function() {
 			self.oauth2unauthorized(true);
 			self.oauth2authorizationURL(this.getAuthorizationURL());
 		};
 	}).handle();
-};
-
-
-// FIXME: remove below
-/**
- * @class the page
- */
-function UIPage () {
-	this.header = new UIHeader(this);
-	this.tasklists = new UITasklists(this);
-	this.calendar = new UICalendar(this);
-	this.refresh();
-}
-/**
- * Refresh the page.
- */
-UIPage.prototype.refresh = function () {
-	var context = this;
-	var defaultTasklistID = undefined;
-	var loadedTasklists = undefined;
-	this.calendar.clear();
-	/**
-	 * Load tasks of the default tasklist.
-	 * @param {Tasks} tasks
-	 */
-	Tasks.get('@default', function (tasks) {
-		if (tasks.items.length > 0) {
-			defaultTasklistID = tasks.items[0].tasklistID;
-		}
-		else {
-			defaultTasklistID = '@default';
-		}
-		context.calendar.add(tasks);
-		context.loadOtherTasks(loadedTasklists, defaultTasklistID);
-	});
-	/**
-	 * Load tasklists.
-	 * @param {Tasklists} tasklists
-	 */
-	Tasklists.get(function (tasklists) {
-		loadedTasklists = tasklists;
-		context.calendar.setTasklists(tasklists);
-		context.tasklists.clear();
-		context.loadOtherTasks(loadedTasklists, defaultTasklistID);
-	});
-	// calendar tab
-	$('a[href="#clear-completed-tasks"]').click(function () {
-		/**
-		 * @param {UITasklist} uiTasklist
-		 */
-		$.each(context.tasklists.getItems(), function (i, uiTasklist) {
-			var elements = uiTasklist.getCompletedTasks();
-			elements.addClass('ajax-in-progress');
-			uiTasklist.getTasklist().clearCompleted(function () {
-				elements.remove();
-			}, function () {
-				elements.removeClass('ajax-in-progress');
-			});
-		});
-		return false;
-	});
-};
-/**
- * Load tasks except tasks of the default tasklist.
- * @param {Tasklists} tasklists
- * @param {String} defaultTasklistID
- */
-UIPage.prototype.loadOtherTasks = function (tasklists, defaultTasklistID) {
-	var context = this;
-	if (defaultTasklistID && tasklists) {
-		$.each(tasklists.items, function (i, tasklist) {
-			if (tasklist.id == defaultTasklistID) {
-				context.tasklists.add(new UITasklist(tasklist, true));
-				$('.task-tasklistID-' + tasklist.id)
-					.addClass('tasklistcolor-' + tasklists.getByID(tasklist.id).colorID);
-			}
-			else {
-				var uiTasklist = new UITasklist(tasklist, false);
-				context.tasklists.add(uiTasklist);
-				uiTasklist.enterAjax();
-				Tasks.get(tasklist.id, function (tasks) {
-					context.calendar.add(tasks);
-					uiTasklist.leaveAjax();
-				});
-			}
-		});
-	}
 };
