3fe9822227485c1f81f68463f3d1aca2af4b11bc
int128 <int128@gmail.com>
2012-05-31 20:38:18 +0900

    split source

--- webapp/js/src/view.calendar.js
+++ webapp/js/src/view.calendar.js
@@ -217,44 +217,6 @@ var TaskViewModel = function (task, tasklist) {
 	this.tasklist = ko.observable(tasklist);
 };
 
-/**
- * @class abstract dialog class
- */
-var Dialog = function () {
-	var self = this;
-	this.visible = ko.observable(false);
-	/**
-	 * Close the dialog.
-	 */
-	this.close = function () {
-		self.visible(false);
-	};
-};
-/**
- * Creating task dialog.
- */
-var CreateTaskDialog = function () {
-	var self = this;
-	this.top = ko.observable();
-	this.due = ko.observable();
-	this.title = ko.observable();
-	this.notes = ko.observable();
-	this.save = function () {
-		// TODO: implement this
-	};
-	/**
-	 * Open the dialog.
-	 * @param {CalendarDayViewModel} dayvm
-	 * @param {Event} event
-	 */
-	this.open = function (dayvm, event) {
-		self.visible(true);
-		self.top(event.pageY + 'px');
-		self.due(dayvm.date());
-	};
-};
-CreateTaskDialog.prototype = new Dialog();
-
 // FIXME: remove below
 /**
  * @class UI element of the calendar.
