54257d6195a7ca8a3b6440539db27d11c46f62d3
int128 <int128@gmail.com>
2012-06-09 15:14:31 +0900

    move extensions

--- webapp/js/src/util.extension.js
+++ webapp/js/src/util.extension.js
@@ -1,3 +1,38 @@
+/**
+ * Get hash code of {@link String}.
+ * @returns {Number} hash code
+ * @see http://werxltd.com/wp/2010/05/13/javascript-implementation-of-javas-string-hashcode-method/
+ */
+String.prototype.hashCode = function () {
+	var h = 0;
+	if (this.length == 0) {
+		return h;
+	}
+	for (var i = 0; i < this.length; i++) {
+		var c = this.charCodeAt(i);
+		h = ((h<<5)-h)+c;
+		h = h & h; // Convert to 32bit integer
+	}
+	return h;
+};
+/**
+ * @class null implementation of web storage
+ * @returns {NullStorage}
+ */
+function NullStorage () {
+	this.length = 0;
+}
+NullStorage.prototype.key = function () {};
+NullStorage.prototype.getItem = function () {};
+NullStorage.prototype.setItem = function () {};
+NullStorage.prototype.removeItem = function () {};
+NullStorage.prototype.clear = function () {};
+if (typeof localStorage === undefined) {
+	window.localStorage = new NullStorage();
+}
+if (typeof sessionStorage === undefined) {
+	window.sessionStorage = new NullStorage();
+}
 /*
  * Array#reduce(function(previousValue, currentValue, index, array))
  * https://developer.mozilla.org/ja/Core_JavaScript_1.5_Reference/Objects/Array/reduce
--- webapp/js/src/util.js
+++ webapp/js/src/util.js
@@ -69,41 +69,6 @@ DateUtil.isThisWeek = function (time) {
 	return first <= time && time < next;
 };
 /**
- * Get hash code of {@link String}.
- * @returns {Number} hash code
- * @see http://werxltd.com/wp/2010/05/13/javascript-implementation-of-javas-string-hashcode-method/
- */
-String.prototype.hashCode = function () {
-	var h = 0;
-	if (this.length == 0) {
-		return h;
-	}
-	for (var i = 0; i < this.length; i++) {
-		var c = this.charCodeAt(i);
-		h = ((h<<5)-h)+c;
-		h = h & h; // Convert to 32bit integer
-	}
-	return h;
-};
-/**
- * @class null implementation of web storage
- * @returns {NullStorage}
- */
-function NullStorage () {
-	this.length = 0;
-}
-NullStorage.prototype.key = function () {};
-NullStorage.prototype.getItem = function () {};
-NullStorage.prototype.setItem = function () {};
-NullStorage.prototype.removeItem = function () {};
-NullStorage.prototype.clear = function () {};
-if (typeof localStorage === undefined) {
-	window.localStorage = new NullStorage();
-}
-if (typeof sessionStorage === undefined) {
-	window.sessionStorage = new NullStorage();
-}
-/**
  * @class Request utility.
  */
 function RequestUtil () {
