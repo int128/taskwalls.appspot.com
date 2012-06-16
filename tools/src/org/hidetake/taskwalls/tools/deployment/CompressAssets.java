e05f792e1ea61fdae77293116fa0a04d43f1dd8a
int128 <int128@gmail.com>
2012-06-17 01:13:30 +0900

    add compressing tool

---
+++ tools/src/org/hidetake/taskwalls/tools/deployment/CompressAssets.java
@@ -0,0 +1,94 @@
+package org.hidetake.taskwalls.tools.deployment;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+import org.apache.commons.io.FileUtils;
+import org.mozilla.javascript.ErrorReporter;
+
+import com.yahoo.platform.yui.compressor.CssCompressor;
+import com.yahoo.platform.yui.compressor.JavaScriptCompressor;
+
+/**
+ * Compress application assets.
+ * 
+ * @author hidetake.org
+ */
+public class CompressAssets {
+
+	private static final String WEBAPP_BASE = "webapp/";
+	private static final String ASSETS_CONF_CSS = "webapp/WEB-INF/assets.css.conf";
+	private static final String ASSETS_CONF_JS = "webapp/WEB-INF/assets.js.conf";
+	private static final String DESTINATION_CSS = "webapp/min.css";
+	private static final String DESTINATION_JS = "webapp/min.js";
+
+	private static final Logger logger = Logger.getLogger(CompressAssets.class.getName());
+
+	public static void main(String[] args) throws Exception {
+		compressCss();
+		compressJs();
+	}
+
+	/**
+	 * Compress CSS sources.
+	 */
+	private static void compressCss() throws Exception {
+		OutputStream destination = FileUtils.openOutputStream(new File(DESTINATION_CSS));
+		try {
+			List<File> sources = new ArrayList<File>();
+			File conf = new File(ASSETS_CONF_CSS);
+			for (String line : FileUtils.readLines(conf)) {
+				sources.add(new File(WEBAPP_BASE + line));
+			}
+			Writer writer = new BufferedWriter(new OutputStreamWriter(destination));
+			for (File file : sources) {
+				logger.info("Compressing " + file.getName());
+				new CssCompressor(new FileReader(file)).compress(writer, -1);
+			}
+			writer.close();
+		} finally {
+			destination.close();
+		}
+	}
+
+	/**
+	 * Compress JavaScript sources.
+	 */
+	private static void compressJs() throws Exception {
+		OutputStream destination = FileUtils.openOutputStream(new File(DESTINATION_JS));
+		try {
+			List<File> asisFiles = new ArrayList<File>();
+
+			File conf = new File(ASSETS_CONF_JS);
+			for (String line : FileUtils.readLines(conf)) {
+				asisFiles.add(new File(WEBAPP_BASE + line));
+			}
+
+			ErrorReporter errorReporter = new YUICompressorErrorReporter();
+			for (File file : asisFiles) {
+				logger.info("Compressing " + file.getName());
+				if (file.getName().endsWith(".min.js")) {
+					// copy as-is
+					FileUtils.copyFile(file, destination);
+					destination.flush();
+				} else {
+					// compress
+					Writer writer = new BufferedWriter(new OutputStreamWriter(destination));
+					new JavaScriptCompressor(new FileReader(file), errorReporter).compress(
+							writer, -1, true, true, false, false);
+					writer.flush();
+				}
+			}
+		} finally {
+			destination.close();
+		}
+	}
+
+}
