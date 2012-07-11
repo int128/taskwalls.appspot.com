a71408387e829955b92313e8c418ab9ef10d0040
int128 <int128@gmail.com>
2012-07-11 10:29:03 +0900

    sanitize inner HTML of task note

--- webapp/js/src/util.ko.js
+++ webapp/js/src/util.ko.js
@@ -93,13 +93,16 @@ ko.bindingHandlers.urlText = {
 		 * @returns
 		 */
 		update: function (element, valueAccessor, allBindingsAccessor, viewModel) {
+			$(element).empty();
 			var text = ko.utils.unwrapObservable(valueAccessor());
 			if (text) {
 				$.each(text.split(/[\r\n]+/), function (i, line) {
-					var div = $(document.createElement('div')).appendTo(element),
-						pattern = /(https?:\/\/[-_.!~*\'()a-zA-Z0-9;\/?:\@&=+\$,%#]+)/g;
-					// FIXME: sanitizing
-					div.html(line.replace(pattern, '<a href="$1">$1</a>'));
+					var div = $(document.createElement('div'));
+					div.text(line);
+					div.html(div.html().replace(
+							/(https?:\/\/[-_.!~*\'()a-zA-Z0-9;\/?:\@&=+\$,%#]+)/g,
+							'<a href="$1" target="_blank">$1</a>'));
+					div.appendTo(element);
 				});
 			}
 		}
