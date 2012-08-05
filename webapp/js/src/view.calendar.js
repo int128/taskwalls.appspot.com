f88e8f51433ba6a4698066c1638e17a3199cff2b
int128 <int128@gmail.com>
2012-08-05 11:26:39 +0900

    fix count of weeks in calendar

--- webapp/js/src/view.calendar.js
+++ webapp/js/src/view.calendar.js
@@ -146,12 +146,12 @@ WeeklyCalendarViewModel.NUMBER_OF_WEEKS = 8;
 WeeklyCalendarViewModel.prototype.initialize = function (taskdata) {
 	// set up weeks in the calendar
 	this.rows = ko.computed(function () {
-		// first day of this week
-		var firstDay = taskwalls.settings.today().getFirstDayOfWeek().getTime();
-		// first day of next week
-		var lastDay = firstDay + WeeklyCalendarViewModel.NUMBER_OF_WEEKS * 7 * 86400000;
-		var rows = [], i = 0;
-		for (var time = firstDay; time <= lastDay; time += 7 * 86400000) {
+		var today = taskwalls.settings.today(),
+			firstWeek = today.getFirstDayOfWeek().getTime(),
+			lastWeek = firstWeek + (WeeklyCalendarViewModel.NUMBER_OF_WEEKS - 1) * 7 * 86400000,
+			rows = [],
+			i = 0;
+		for (var time = firstWeek; time <= lastWeek; time += 7 * 86400000) {
 			rows[i++] = new WeeklyCalendarViewModel.Row(time);
 		}
 		return rows;
